8f0950e0d682327b346d9ce7e598a096dc52fe54
piyachadJPT <piyachad.jpt@gmail.com>
2025-05-05 17:30:04 +0700

    .

--- config/omiseConfig.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-const omiseConfig = {
-    publicKey: process.env.OMISE_PUBLIC_KEY,
-    secretKey: process.env.OMISE_SECRET_KEY,
-};
-
-export default omiseConfig;
